37537540e58ce5c7f831adac52a55363f0d252bd
AvdushkinaKsenia <58066567+AvdushkinaKsenia@users.noreply.github.com>
2023-09-16 20:32:25 +0600

    Add files via upload

---
+++ OL/Gryadki/Cycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        Console.WriteLine("Cycle");
+        Console.WriteLine("Write k: ");
+        int k = Convert.ToInt32(Console.ReadLine());
+        int l = 7;
+        int n = 5;
+        int m = 10;
+        int pathLenght = 0;
+
+        for (int i = 1; i <= k; i++)
+        {
+            pathLenght += l * 2 + 2 * n + 2 * m * i;
+        }
+
+        Console.WriteLine(pathLenght);
+    }
+}
